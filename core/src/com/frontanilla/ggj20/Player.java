3c9e7da6f81b2855d2d2402ad68c5ad859fb2393
Luis Frontanilla <lrdolucho@gmail.com>
2020-02-01 23:33:00 -0400

    Velocity

--- core/src/com/frontanilla/ggj20/Player.java
+++ core/src/com/frontanilla/ggj20/Player.java
@@ -2,15 +2,18 @@ package com.frontanilla.ggj20;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 class Player {
 
     private TextureRegion region;
     private Rectangle bounds;
+    private Vector2 velocity;
 
     Player(TextureRegion region) {
         this.region = region;
         bounds = new Rectangle();
+        velocity = new Vector2();
     }
 
     TextureRegion getRegion() {
@@ -19,5 +22,9 @@ class Player {
 
     Rectangle getBounds() {
         return bounds;
+    }
+
+    public Vector2 getVelocity() {
+        return velocity;
     }
 }
