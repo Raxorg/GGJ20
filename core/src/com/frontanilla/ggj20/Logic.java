e454e01175bff4d675f3e99635b42ef8f873c451
Luis Frontanilla <lrdolucho@gmail.com>
2020-02-01 23:42:15 -0400

    Logic

---
+++ core/src/com/frontanilla/ggj20/Logic.java
@@ -0,0 +1,20 @@
+package com.frontanilla.ggj20;
+
+class Logic {
+
+    private Movement movement;
+    // Other
+    private Stuff stuff;
+
+    Logic() {
+        movement = new Movement();
+    }
+
+    void spacePressed() {
+        stuff.getPlayer().getVelocity().y = 100f;
+    }
+
+    void setStuff(Stuff stuff) {
+        this.stuff = stuff;
+    }
+}
--- core/src/com/frontanilla/ggj20/SpaceshipGame.java
+++ core/src/com/frontanilla/ggj20/SpaceshipGame.java
@@ -1,16 +1,22 @@
 package com.frontanilla.ggj20;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
 public class SpaceshipGame extends Game {
 
     public void create() {
+        Logic logic = new Logic();
         Stuff stuff = new Stuff();
         Screen screen = new Screen();
 
-        stuff.init();
+        logic.setStuff(stuff);
         screen.setStuff(stuff);
 
+        stuff.init();
+
         setScreen(screen);
+
+        Gdx.input.setInputProcessor(new Input(this));
     }
 }
