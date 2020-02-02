be8f4495124b015941d6930725e71f0d63ff6067
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2020-02-02 01:36:29 -0400

    HombreLobo1 Assets

--- core/src/com/frontanilla/ggj20/Assets.java
+++ core/src/com/frontanilla/ggj20/Assets.java
@@ -6,8 +6,10 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class Assets {
 
-    public Texture tail, armR, armL, forearmR, forearmL, head, body, thightR, thighL, legR, legL;
-
+    public Texture hombreloboMutante,
+                   indigena,
+                   indigenaMutante,
+                   LoboMutante;
     public AssetManager assetManager;
 
     public  Assets(){
@@ -16,78 +18,33 @@ public class Assets {
 
     void loadAssets(){
         //Load Hombre Lobo
-        assetManager.load("hombreLobo/cola.png", Texture.class);
-        assetManager.load("hombreLobo/antebrazoD.png", Texture.class);
-        assetManager.load("hombreLobo/antebrazoI.png", Texture.class);
-        assetManager.load("hombreLobo/brazoD.png", Texture.class);
-        assetManager.load("hombreLobo/brazoI.png", Texture.class);
-        assetManager.load("hombreLobo/cabeza.png", Texture.class);
-        assetManager.load("hombreLobo/cuerpo.png", Texture.class);
-        assetManager.load("hombreLobo/musloD.png", Texture.class);
-        assetManager.load("hombreLobo/musloI.png", Texture.class);
-        assetManager.load("hombreLobo/piernaD.png", Texture.class);
-        assetManager.load("hombreLobo/piernaI.png", Texture.class);
+        assetManager.load("hombreLoboMutante.png", Texture.class);
+        assetManager.load("indigena.png", Texture.class);
+        assetManager.load("indigenaMutante.png", Texture.class);
+        assetManager.load("LoboMutante.png", Texture.class);
 
         assetManager.finishLoading();
 
         //Get Hombre lobo
-        tail = assetManager.get("hombreLobo/cola.png", Texture.class);
-        armR = assetManager.get("hombreLobo/antebrazoD.png", Texture.class);
-        armL = assetManager.get("hombreLobo/antebrazoI.png", Texture.class);
-        forearmR = assetManager.get("hombreLobo/brazoD.png", Texture.class);
-        forearmL = assetManager.get("hombreLobo/brazoI.png", Texture.class);
-        head = assetManager.get("hombreLobo/cabeza.png", Texture.class);
-        body = assetManager.get("hombreLobo/cuerpo.png", Texture.class);
-        thightR = assetManager.get("hombreLobo/musloD.png", Texture.class);
-        thighL = assetManager.get("hombreLobo/musloI.png", Texture.class);
-        legR = assetManager.get("hombreLobo/piernaD.png", Texture.class);
-        legL = assetManager.get("hombreLobo/piernaI.png", Texture.class);
-
-
+        hombreloboMutante = assetManager.get("hombreLoboMutante.png", Texture.class);
+        indigena = assetManager.get("indigena.png", Texture.class);
+        indigenaMutante = assetManager.get("indigenaMutante.png", Texture.class);
+        LoboMutante = assetManager.get("LoboMutante.png", Texture.class);
     }
 
-    //getter Hombre Lobo
-    public Texture getTail() {
-        return tail;
+    public Texture getHombreloboMutante() {
+        return hombreloboMutante;
     }
 
-    public Texture getArmR() {
-        return armR;
+    public Texture getIndigena() {
+        return indigena;
     }
 
-    public Texture getArmL() {
-        return armL;
+    public Texture getIndigenaMutante() {
+        return indigenaMutante;
     }
 
-    public Texture getForearmR() {
-        return forearmR;
-    }
-
-    public Texture getForearmL() {
-        return forearmL;
-    }
-
-    public Texture getHead() {
-        return head;
-    }
-
-    public Texture getBody() {
-        return body;
-    }
-
-    public Texture getThightR() {
-        return thightR;
-    }
-
-    public Texture getThighL() {
-        return thighL;
-    }
-
-    public Texture getLegR() {
-        return legR;
-    }
-
-    public Texture getLegL() {
-        return legL;
+    public Texture getLoboMutante() {
+        return LoboMutante;
     }
 }
